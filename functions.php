3839366fbe6f98083f61d7481a547776b8b427b2
Andrea Ripamonti <andrea.ripamonti@b2bit.it>
2025-09-23 12:17:20 +0200

    assets for slick in fx.php

--- functions.php
+++ functions.php
@@ -28,14 +28,39 @@ function main_styles() {
         get_stylesheet_directory_uri() . '/assets/fonts/custom-icons/css/custom-icons.css', 
         array()
     );
+    wp_register_style( 
+        'slick', 
+        get_stylesheet_directory_uri() . '/assets/packages/slick-1.8.1/slick/slick.css',
+        array(), 
+        '1.8.1',
+    );
 }
 add_action( 'wp_enqueue_scripts', 'main_styles' );
 
+function main_scripts() {
+    wp_register_script( 
+        'slick', // ID, NOME UNIVOCO, HANDLE
+        get_stylesheet_directory_uri() . '/assets/packages/slick-1.8.1/slick/slick.min.js', // Percorso
+        array('jquery'), // Dipendenze
+        '1.8.1', // versione
+        true // true = nel footer, false = nell'header
+    );
+    wp_register_script( 
+        'carousel', 
+        get_stylesheet_directory_uri() . '/blocks/carousel/carousel.js',
+        array('slick'), 
+        '1.0', 
+        true
+    );
+}
+add_action( 'wp_enqueue_scripts', 'main_scripts' );
+
 if( class_exists('acf') ) {
     function register_acf_blocks() {
-        register_block_type( __DIR__ . '/blocks/testimonial' );
+        register_block_type( __DIR__ . '/blocks/carousel' );
         register_block_type( __DIR__ . '/blocks/personal-card' );
         register_block_type( __DIR__ . '/blocks/progress-bar' );
+        register_block_type( __DIR__ . '/blocks/testimonial' );
     }
     add_action('init', 'register_acf_blocks');
 }
