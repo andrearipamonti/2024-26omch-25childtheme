fcfee910105844917df36d967281f373c635253c
Andrea Ripamonti <andrea.ripamonti@b2bit.it>
2025-09-29 09:30:05 +0200

    Aggiunto il blocco "Currently Listening" con file di configurazione, stili e script; aggiornato il TODO per includere il blocco "Weather Widget"

---
+++ blocks/currently-listening/currently-listening.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * 
+ * Crea un blocco che mostra cosa si sta ascoltando in questo momento
+ * 
+ * Questo blocco è avanzato da fare, prova a farlo come ULTIMO blocco custom 
+ * 
+ * Cosa ti serve:
+ * Puoi farlo con solo le API di Spotify oppure con le API di Last.fm
+ * 
+ * Se usi Spotify significa che funzionerà solo se l'utente ha un account Spotify
+ * Se usi Last.fm funziona per tutti, ma l'utente deve aver collegatoo il suo account Spotify, apple Music o altro servizio streaming a Last.fm
+ * 
+ * Segui le doc ufficiali:
+ * Spotify: https://developer.spotify.com/documentation/web-api
+ * Last.fm: https://www.last.fm/api
+ * 
+ * Sarà necessario creare un'applicazione per ottenere le API key
+ * Usa ACF per permettere all'utente di inserire le API key
+ * Usa ACF per permettere all'utente di inserire il nome utente Spotify o Last.fm
+ * * Il blocco deve mostrare:
+ * - se l'utente sta ascoltando qualcosa in questo momento, mostra titolo canzone, artista, copertina album
+ * - se l'utente NON sta ascoltando nulla in questo momento, non devo mostrare nulla
+ * 
+ * 
+ * */
