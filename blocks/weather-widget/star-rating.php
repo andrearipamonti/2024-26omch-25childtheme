4e49c2b88d1fefc467c29429427cc0efde0b8f4a
Andrea Ripamonti <andrea.ripamonti@b2bit.it>
2025-09-29 09:17:02 +0200

    Aggiunto il blocco weather-widget con file di configurazione, stili e script

---
+++ blocks/weather-widget/star-rating.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * 
+ * Crea un blocco che possa mostare le condizioni meteo in piccolo
+ * 
+ * Deve mostare temperatura e icona che rappresenta la condizione meteo della poszione attuale ottenute tramite browser
+ * Usa openweathermap per ottenere i dati meteo
+ * Usa JS per ottenere la posizione attuale dell'utente
+ * 
+ * */
